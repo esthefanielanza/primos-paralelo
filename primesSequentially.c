1e7d59a0d3bddcfe9234511a26b4ec8f076f2407
Esthefanie Lanza <esthefanielanza@gmail.com>
2019-08-20 23:35:10 -0300

    Added IS_NOT_PRIME variable to make it more readable

--- primesSequentially.c
+++ primesSequentially.c
@@ -1,21 +1,25 @@
 void calculatePrimesSequentially(int max) {
-  char *numbers = (char *)calloc(max, sizeof(char));
   int FIRST_PRIME = 2;
   int LAST_NUMBER_TO_TEST_MULTIPLES = max/2 + 1;
+  int IS_NOT_PRIME = 1;
+
+  char *numbers = (char *)calloc(max, sizeof(char));
+
+
 
   for(int i = FIRST_PRIME; i < LAST_NUMBER_TO_TEST_MULTIPLES; i++) {
     int currentMultiple = i;
 
-    if(numbers[currentMultiple] != 1) {
+    if(numbers[currentMultiple] != IS_NOT_PRIME) {
       while(currentMultiple + i < max) {
         currentMultiple = currentMultiple + i;
-        numbers[currentMultiple] = 1;
+        numbers[currentMultiple] = IS_NOT_PRIME;
       }
     }
   }
 
   for(int i = FIRST_PRIME; i < max; i++) {
-    if(numbers[i] == 0) {
+    if(numbers[i] != IS_NOT_PRIME) {
       printf("%d, ", i);
     }
   }
