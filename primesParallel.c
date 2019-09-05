3e08c2497c068761c5dcc47cbb4fb0bc8c0f4d7c
Esthefanie Lanza <esthefanielanza@gmail.com>
2019-09-04 22:59:38 -0300

    Revert "Calculate thread times, will revert this"
    This reverts commit 26346982d7c376cb2d4b7c77a2795340e409a47c.

--- primesParallel.c
+++ primesParallel.c
@@ -1,5 +1,4 @@
 #include "primesParallel.h"
-#include <omp.h>
 
 int FIRST_PRIME = 2;
 int IS_PRIME = 0;
@@ -22,26 +21,10 @@ void calculatePrimesParallel(int max, char *list, int threads){
 
   int currentMultiple;
   
-  double *l_time = (double *)calloc(threads, sizeof(double));
-
-  double t_timer;
-  int this_thread;
-
   #pragma omp parallel for num_threads(threads) schedule(dynamic, 2)
   for(currentMultiple = FIRST_PRIME; currentMultiple < LAST_NUMBER_TO_TEST_MULTIPLES; currentMultiple++) {
-    
-    t_timer = omp_get_wtime();
-    this_thread = omp_get_thread_num();
-      
     if(list[currentMultiple] == IS_PRIME) {
       calculateMultiples(max, list, currentMultiple);
     }
-
-    l_time[this_thread] += omp_get_wtime() - t_timer;
-  }
-
-  int number;
-  for(number = 0; number < threads; number++) {
-    printf("Thread %d time %.8lf\n", number, l_time[number]);
   }
 }
